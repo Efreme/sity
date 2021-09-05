651336e3886ee07f940acd0b53dd18baa57139b0
Efreme <89541108+Efreme@users.noreply.github.com>
2021-09-05 18:08:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,76 +1,59 @@
 
-const BifImage = document.getElementById('photo');
-console.log(BifImage);
-
-//for (let index = 0; index < BifImage.length; index++) 
-//    {
-//    const inputBigImage = BifImage[index];
-//    console.log(inputBigImage);  
-
-BifImage.onclick = function(){
+$(function() {
     
-      
-    BifImage.classList.add('_active');
-
-}
+   let header = $("#header");
+    let intro = $("#intro")
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    
+    $(window).on("scroll load resize", function(){              
+       
+        scrollPos = $(this).scrollTop()
+        
+        if( scrollPos > introH) {
+            
+           header.addClass("fixed"); 
+            
+        } else {
+            header.removeClass("fixed");
+        }
+        
+        console.log(scrollPos);
+    
+    });
+    
+    
+    
+    
    
-
-//
-//$(function() {
-//    
-//   let header = $("#header");
-//    let intro = $("#intro")
-//    let introH = intro.innerHeight();
-//    let scrollPos = $(window).scrollTop();
-//    
-//    $(window).on("scroll load resize", function(){              
-//       
-//        scrollPos = $(this).scrollTop()
-//        
-//        if( scrollPos > introH) {
-//            
-//           header.addClass("fixed"); 
-//            
-//        } else {
-//            header.removeClass("fixed");
-//        }
-//        
-//        console.log(scrollPos);
-//    
-//    });
-//    
-//    
-//    
-//    
-//   
-//    
-//    
-//    $("[data-scroll]").on("click", function(event)
-//    {
-//        event.preventDefault();
-//        
-//        let elementId = $(this).data('scroll');
-//        
-//        let elementOffset = $(elementId).offset().top;
-//        
-//        console.log(elementOffset);
-//        
-//        $("html, body").animate({
-//            scrollTop: elementOffset - 110
-//        }, 1000);
-//    });
-//    
-//    
-//    let slider = $("#reviewsSlider");
-//    slider.slick({
-//    infinite: true,
-//    slidesToShow: 1,
-//    slidesToScroll: 1,
-//    fade: true,
-//    arrows: false,
-//        dots: true
-//});
-//    
-//    
-//    
-//});
+    
+    
+    $("[data-scroll]").on("click", function(event)
+    {
+        event.preventDefault();
+        
+        let elementId = $(this).data('scroll');
+        
+        let elementOffset = $(elementId).offset().top;
+        
+        console.log(elementOffset);
+        
+        $("html, body").animate({
+            scrollTop: elementOffset - 110
+        }, 1000);
+    });
+    
+    
+    let slider = $("#reviewsSlider");
+    slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+        dots: true
+});
+    
+    
+    
+});
