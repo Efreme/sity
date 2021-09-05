cf9e6cec99404041b1b10c1b00fc1dc2fb552038
Efreme <89541108+Efreme@users.noreply.github.com>
2021-09-05 18:02:46 +0300

    Add files via upload

--- PHPMailer/sendmail.php
+++ PHPMailer/sendmail.php
@@ -1,79 +1,71 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
 
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->IsHTML(true);
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
 
-//От кого письмо
+	//От кого письмо
+	$mail->setFrom('tujhtahtvjd@gmail.com', 'Фрилансер по жизни');
+	//Кому отправить
+	$mail->addAddress('efremev53@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Привет! Твоя форма"';
 
-$mail->setFrom('info@fls.guru', 'Егор Ефремов');
+	//Рука
+	$hand = "Правая";
+	if($_POST['hand'] == "left"){
+		$hand = "Левая";
+	}
 
-//Кому отправить
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))){
+		$body.='<p><strong>Сосал или ебали:</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Лучший брат:</strong> '.$_POST['age'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
 
-$mail->addAddress('code@fls.guru');
+	$mail->Body = $body;
 
-//Тема письма
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
 
-$mail->Subject = 'Привет! Это твоя форма !';
+	$response = ['message' => $message];
 
-
-//Pyka
-
-$hand = "Правая";
-if($_POST['hand'] == "left" ){
-    $hand = "Левая";
-}
-
-
-//Тело письма
-
-$body = '<h1>Встречайте суперь письмо!</h1>';
-
-if(trim(!empty($_POST['name']))){
-    $body.='<p><strong>Имя:</strong>'.$_POST['name'].'</p>';
-}
-if(trim(!empty($_POST['email']))){
-    $body.='<p><strong>E-mail:</strong>'.$_POST['email'].'</p>';
-}
-if(trim(!empty($_POST['hand']))){
-    $body.='<p><strong>Рука:</strong>'.$hand.'</p>';
-}
-if(trim(!empty($_POST['age']))){
-    $body.='<p><strong>Возраст:</strong>'.$_POST['age'].'</p>';
-}
-if(trim(!empty($_POST['message']))){
-    $body.='<p><strong>Сообщение:</strong>'.$_POST['message'].'</p>';
-}
-
-//Прикрепить файл
-if (!empty($_FILES['image']['tmp_name'])) {
-//    пусть загрузки файла
-    $filePath = _DIR_ . "/files/" . $_FILES['image']['name'];
-//    грузим файл
-    if(copy($_FILES['image']['tmp_name'], $filePath)) {
-        $fileAttach = $filePath;
-        $body.='<p><strong>Фото в приложении</strong>';
-        $mail->addAttachment($fileAttach);
-    }
-}
-$mail->Body = $body;
-
-//Отправляем
-
-if(!$mail->send()) {
-    $message = 'Ошибка!';
-} else {
-    $message = 'Данные отправлены!';
-}
-
-$response = ['message' => $message];
-
-header('Content-type: application/json');
-echo json_encode($response);
+	header('Content-type: application/json');
+	echo json_encode($response);
 ?>
